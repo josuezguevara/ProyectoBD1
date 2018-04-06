90e8516f2c9a7cae3d976a05b150ba0a3ff4e629
josuezguevara <josuezelaya99@hotmail.es>
2018-04-05 20:33:02 -0600

    ME DIO LA CONEXION YAAAY

---
+++ ProyectoBd1/src/proyectobd1/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectobd1;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Josué Zelaya
+ */
+public class Conexion {
+     
+    public Connection conectar(){
+        String url="jdbc:sqlserver://DESKTOP-BBK328O:1433;databaseName=Proyecto";
+        String pass="1234";
+        String us="josue";
+        Connection cn=null;
+        try {
+            
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            cn=DriverManager.getConnection(url, us, pass);
+            JOptionPane.showMessageDialog(null, "Conexion con la conexion de bd");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error con la conexion de bd"+e);
+        }
+        return cn;
+    }
+}
